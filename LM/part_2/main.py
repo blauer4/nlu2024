cf9ce8242e3020ff9d97712c7aff0c990356ea70
Laurence Bonat <laurence.bonat@gmail.com>
2024-04-15 17:08:44 +0200

    refactor: Part 2 run all experiments

--- LM/part_2/main.py
+++ LM/part_2/main.py
@@ -7,8 +7,8 @@ from functions import *
 DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
 if __name__ == "__main__":
-    to_run = {4: {'name': 'LSTM_WEIGHT_TYING_SGD', 'run': False},
-              5: {'name': 'LSTM_WEIGHT_TYING_DROPOUT_SGD', 'run': False},
+    to_run = {4: {'name': 'LSTM_WEIGHT_TYING_SGD', 'run': True},
+              5: {'name': 'LSTM_WEIGHT_TYING_DROPOUT_SGD', 'run': True},
               6: {'name': 'LSTM_WEIGHT_TYING_DROPOUT_AVSGD', 'run': True}}
 
     run_experiments(to_run)
